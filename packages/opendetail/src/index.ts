8d250f5db830fe05ff5a7948e68f36ccb0281ea5
handleui <rodrigo@handleui.com>
2026-04-06 23:22:35 -0600

    fux: stale integration mode

--- packages/opendetail/src/index.ts
+++ packages/opendetail/src/index.ts
@@ -40,7 +40,6 @@ export type {
   OpenDetailImage,
   OpenDetailImagesEvent,
   OpenDetailIndexArtifact,
-  OpenDetailIntegrationMode,
   OpenDetailMediaConfig,
   OpenDetailMetaEvent,
   OpenDetailPublicError,
